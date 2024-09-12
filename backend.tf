a3e8101b739a8dcd100976cc2567b7694db5076d
arinfinity <160311649+arinfinity@users.noreply.github.com>
2024-09-12 20:06:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "akshaya-project"
-    key = "main"
+    key = "path/to/terraform.tfstate"
     region = "ap-southeast-1"
     dynamodb_table = "statelock"
   }
